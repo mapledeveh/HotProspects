54e5870afd2d74d6ed66d73a361c4a67b77188d9
mapledeveh <mapledeveh@gmail.com>
2023-08-07 11:21:54 -0400

    Update Progress

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Alex Nguyen on 2023-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedTab = "One"
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Tab One")
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+                .tag("One")
+            Text("Tab Two")
+                .onTapGesture {
+                    selectedTab = "One"
+                }
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Alex Nguyen on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
