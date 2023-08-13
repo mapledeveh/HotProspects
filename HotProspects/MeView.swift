c65f5d72c1e439802063df30e7178d5b821bb03f
Alex Nguyen <alexviean@gmail.com>
2023-08-12 23:59:27 -0400

    Update

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -6,10 +6,47 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var email = "anonymous@emailaddress.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+            
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
     var body: some View {
-        Text("Hello, me!")
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                TextField("Email", text: $email)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(email)"))
+                    .resizable()
+                    .interpolation(.none)
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your Code")
+        }
     }
 }
 
